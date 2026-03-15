48d2cf31070241e9cbfb683008a58f01bf6bcf4d
GalkinAlexeyAndreevich <sir.methews@gmail.com>
2026-03-15 16:49:28 +0300

    feat: init ui-kit on lit

---
+++ src/my-element.ts
@@ -0,0 +1,27 @@
+import { LitElement, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import './ui-button'
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+  @property({ type: Number })
+  count = 0
+
+  render() {
+    return html`
+        <ui-button class="counter" @click=${this._onClick} part="button">
+          Count is ${this.count}
+        </ui-button>
+    `
+  }
+
+  private _onClick() {
+    this.count++
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'my-element': MyElement
+  }
+}
