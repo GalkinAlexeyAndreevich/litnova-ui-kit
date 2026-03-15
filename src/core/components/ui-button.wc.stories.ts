8611d09b809e3c827de691de655c5052dbe23a1b
GalkinAlexeyAndreevich <sir.methews@gmail.com>
2026-03-15 17:04:49 +0300

    feat: add storybook base configuration

---
+++ src/core/components/ui-button.wc.stories.ts
@@ -0,0 +1,13 @@
+import './ui-button';
+
+export default {
+  title: 'Components/Button/Web Component',
+};
+
+export const Primary = () => `
+  <ui-button variant="primary">Click me</ui-button>
+`;
+
+export const Danger = () => `
+  <ui-button variant="danger">Click me</ui-button>
+`;
